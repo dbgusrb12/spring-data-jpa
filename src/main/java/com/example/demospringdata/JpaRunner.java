48e98eb2e053a4eece38276070b9de54a1d84ef3
hyungyu <yhg8484@naver.com>
2021-12-21 15:47:11 +0900

    cascadetype sample 추가

--- src/main/java/com/example/demospringdata/JpaRunner.java
+++ src/main/java/com/example/demospringdata/JpaRunner.java
@@ -16,24 +16,64 @@ public class JpaRunner implements ApplicationRunner {
     @PersistenceContext
     EntityManager entityManager;
 
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//        // Transient 상태 (객체를 생성 하고, JPA 가 모르는 상태)
+//        Account account = new Account();
+//        account.setUsername("hyungyu");
+//        account.setPassword("jpa");
+//
+//        Study study = new Study();
+//        study.setName("Spring Data JPA");
+//
+//        // 객체 지향 적인 코드를 위해 두 객체를 서로 매핑 해주는 메서드를 만든다.
+//        account.addStudy(study);
+//
+//        // jpa 사용
+////        entityManager.persist(account);
+//
+//        // hibernate api 사용
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        // Persistent 상태 (JPA 가 객체를 인식하고 관리중인 상태)
+//        session.save(account);
+//        session.save(study);
+//
+//        Account load = session.load(Account.class, account.getId());
+//        load.setUsername("springboot");
+//        load.setUsername("springboot2");
+//        load.setUsername("springboot3");
+//        load.setUsername("hyungyu");
+//        System.out.println("======================");
+//        System.out.println(load.getUsername());
+//    }
+
+    /**
+     * cascade 옵션 확인 용 메서드
+     * @param args
+     * @throws Exception
+     */
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Account account = new Account();
-        account.setUsername("hyungyu");
-        account.setPassword("jpa");
+        // CascadeType.PERSIST
+//        Post post = new Post();
+//        post.setTitle("Spring Data JPA 강의중...");
+//
+//        Comment comment = new Comment();
+//        comment.setComment("강의 듣는중.");
+//        post.addComment(comment);
+//
+//        Comment comment1 = new Comment();
+//        comment1.setComment("Spring Data JPA는 아직 못봄");
+//        post.addComment(comment1);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.save(post);
 
-        Study study = new Study();
-        study.setName("Spring Data JPA");
-
-        // 객체 지향 적인 코드를 위해 두 객체를 서로 매핑 해주는 메서드를 만든다.
-        account.addStudy(study);
-
-        // jpa 사용
-//        entityManager.persist(account);
-
-        // hibernate api 사용
+        // CascadeType.REMOVE
         Session session = entityManager.unwrap(Session.class);
-        session.save(account);
-        session.save(study);
+        Post post = session.get(Post.class, 1l);
+
+        session.delete(post);
     }
 }
---
+++ src/main/java/com/example/demospringdata/Post.java
@@ -0,0 +1,51 @@
+package com.example.demospringdata;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Post {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    // cascade 로 entity의 상태를 전파 시킬 수 있다. (어떤 상태를 전파시킬건지 정의 할 수 있음)
+    // 해당 Post 객체가 persistent 상태로 변경 될 때 참조되는 Comment 리스트 객체들도 persistent 상태로 전파된다.
+    // 해당 Post 객체가 removed 상태로 변경 될 때 참조되는 Comment 리스트 객체들도 removed 상태로 전파된다.
+//    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    // 모든 상태를 전파 시키는 옵션
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private Set<Comment> comments = new HashSet<>();
+
+    public void addComment(Comment comment) {
+        this.getComments().add(comment);
+        comment.setPost(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+}
