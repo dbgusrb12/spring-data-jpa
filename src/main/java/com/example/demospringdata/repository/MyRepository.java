94dbc08baaafa628695c28db9462bab2e0e4403e
hyungyu <yhg8484@naver.com>
2021-12-23 18:12:27 +0900

    Null 처리 관련 추가

--- src/main/java/com/example/demospringdata/repository/MyRepository.java
+++ src/main/java/com/example/demospringdata/repository/MyRepository.java
@@ -2,9 +2,12 @@ package com.example.demospringdata.repository;
 
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * custom 하게 작성한 메서드들을 공통으로 모아서 사용 할 수 있다.
@@ -14,8 +17,11 @@ import java.util.List;
 @NoRepositoryBean
 public interface MyRepository<T, ID extends Serializable> extends Repository<T, ID> {
 
-    <E extends T> E save(E comment);
+    <E extends T> E save(@NonNull E comment);
 
     List<T> findAll();
+
+    @Nullable
+    <E extends T> Optional<E> findById(ID id);
 
 }
--- src/test/java/com/example/demospringdata/repository/AccountRepositoryTest.java
+++ src/test/java/com/example/demospringdata/repository/AccountRepositoryTest.java
@@ -1,18 +1,16 @@
 package com.example.demospringdata.repository;
 
 import com.example.demospringdata.entity.Account;
-import com.example.demospringdata.entity.Comment;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -21,7 +19,6 @@ public class AccountRepositoryTest {
     AccountRepository accountRepository;
 
     @Test
-    @Rollback(false)
     public void crud() {
         // Given
         Account account = new Account();
@@ -34,5 +31,13 @@ public class AccountRepositoryTest {
         // Then
         List<Account> all = accountRepository.findAll();
         assertThat(all.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void optionalTest() {
+        Optional<Account> byId = accountRepository.findById(100l);
+        assertThat(byId).isEmpty();
+//        Account account = byId.orElseThrow(IllegalArgumentException::new);
+
     }
 }
