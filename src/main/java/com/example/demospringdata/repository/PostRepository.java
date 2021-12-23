a0884fdd4f734f9763425ad95c752ab5ba370693
hyungyu <yhg8484@naver.com>
2021-12-23 16:48:19 +0900

    test 코드 추가

--- src/main/java/com/example/demospringdata/repository/PostRepository.java
+++ src/main/java/com/example/demospringdata/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.example.demospringdata.repository;
 
 import com.example.demospringdata.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -9,4 +11,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    // 쿼리 메서드를 지원 해준다.
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    // 메서드 명으로 다양한 쿼리를 만들 수 있다.
+    long countByTitleContains(String title);
 }
